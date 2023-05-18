ad11f60a12cc55da99de399e49e8c37306350418
David Basile Filho <39467981+davidbasilefilho@users.noreply.github.com>
2023-05-18 15:38:37 -0300

    movimentacao simples sem ground check, porque estamos em aula e nao tem unity aqui para testar

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private bool jumpRequest;
+    private float dir;
+    private actualSpeed;
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement() {
+        actualSpeed = dir * speed;
+
+        rb.velocity = new Vector2(actualSpeed, rb.velocity.y);
+    }
+
+    void HandleJump() {
+        if (jumpRequest) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpRequest = false;
+        }
+    }
+
+
+    public void SetDirection(float d) => dir = d;
+    
+    public void Jump() => jumpRequest = true;
+}
